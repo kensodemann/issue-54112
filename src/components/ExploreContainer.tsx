76c26f5e4cb4cfc5912faa1561615b76540fd586
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-07-22 08:12:48 -0500

    show a popover

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,3 +1,4 @@
+import { IonButton, IonContent, IonInput, IonItem, IonList, IonPopover } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps {}
@@ -5,13 +6,46 @@ interface ContainerProps {}
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>
-        Start with Ionic{' '}
-        <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">
-          UI Components
-        </a>
-      </p>
+      <IonList>
+        <IonItem button={true} detail={false}>
+          External Option 1
+        </IonItem>
+        <IonItem button={true} detail={false}>
+          External Option 2
+        </IonItem>
+        <IonItem button={true} detail={false}>
+          <IonInput label="let's try an input"></IonInput>
+        </IonItem>
+      </IonList>
+      <IonButton id="popover-button">Open Menu</IonButton>
+      <IonPopover trigger="popover-button" dismissOnSelect={true}>
+        <IonContent>
+          <IonList>
+            <IonItem button={true} detail={false}>
+              Option 1
+            </IonItem>
+            <IonItem button={true} detail={false}>
+              Option 2
+            </IonItem>
+            <IonItem button={true} detail={false}>
+              <IonInput label="Input"></IonInput>
+            </IonItem>
+            <IonItem button={true} id="nested-trigger">
+              More options...
+            </IonItem>
+
+            <IonPopover trigger="nested-trigger" dismissOnSelect={true} side="end">
+              <IonContent>
+                <IonList>
+                  <IonItem button={true} detail={false}>
+                    Nested option
+                  </IonItem>
+                </IonList>
+              </IonContent>
+            </IonPopover>
+          </IonList>
+        </IonContent>
+      </IonPopover>
     </div>
   );
 };
